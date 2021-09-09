8ccc2a820a0891c6a7553866d3e9b4d29d5e8ae4
SungmiKim <62002100+SungmiKim@users.noreply.github.com>
2021-09-09 23:21:00 +0900

    헤더 반응형 완료

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,36 @@
 // 헤더 메뉴
 function showMenu() {
   if ($("#header .header_area").height() <= 100) {
-    $("#header .header_area").stop().animate({ height: "426px" }, "350");
+    var windowWidth = $(window).width();
+    if (windowWidth <= 960) {
+      $("#header .header_area").stop().animate({ height: "100vh" }, "350");
+    } else {
+      $("#header .header_area").stop().animate({ height: "426px" }, "350");
+    }
     $("#header .header_area").addClass("on");
     $("#header .header_area").addClass("active");
     $(".menu_bg").fadeIn();
   }
 }
 
+function closeMenu() {
+  if ($("#header .header_area").height() > 100) {
+    $("#header .header_area").stop().animate({ height: "100px" }, 150);
+    $(".menu_bg").fadeOut();
+    $("#header .header_area").removeClass("on");
+    $("#menu .menu_step01 .wrap").removeClass("show");
+    if (swiper.realIndex === 0) {
+      $("#header .header_area").removeClass("active");
+    }
+  }
+}
+// 모바일 메뉴
+function dropMoMenu(num) {
+  $("#menu .menu_step01 .wrap:not(.menu0" + num + " .wrap)").removeClass(
+    "show"
+  );
+  $("#menu .menu0" + num + " .wrap").toggleClass("show");
+}
 // 언어 선택
 function openLang() {
   $(".balloon").toggleClass("balloon_show");
@@ -20,16 +43,16 @@ $(document).ready(function () {
   });
   $(".side_btn .munu_min").bind("click", function () {
     showMenu();
+    var windowWidth = $(window).width();
+    if (windowWidth <= 960) {
+      closeMenu();
+    }
   });
 
   $("#header .header_area").bind("mouseleave", function () {
-    if ($("#header .header_area").height() > 100) {
-      $("#header .header_area").stop().animate({ height: "100px" }, 150);
-      $(".menu_bg").fadeOut();
-      $("#header .header_area").removeClass("on");
-      if (swiper.realIndex === 0) {
-        $("#header .header_area").removeClass("active");
-      }
+    var windowWidth = $(window).width();
+    if (windowWidth > 960) {
+      closeMenu();
     }
   });
 
@@ -43,6 +66,16 @@ $(document).ready(function () {
       $(".balloon").removeClass("balloon_show");
     }
   });
+
+  // 모바일 헤더 메뉴
+  // $("#menu .menu_step01 .wrap span").click(function () {
+
+  //   if ($(this).parent().siblings(".sub_wrap").css("display", "block")) {
+  //     $(this).parent().removeClass("show");
+  //   } else {
+  //     $(this).parent().addClass("show");
+  //   }
+  // });
 
   //  Footer 관련 사이트
   $(".dropdown").click(function () {
